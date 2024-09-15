c57f72219094dbaec9bbf0cbacde092321441037
Alexander-Agu <theonlyagu@gmail.com>
2024-09-15 12:59:04 +0200

    Created the header, made it responsive and gave it basic styling

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,49 @@
 import React from 'react'
+import './header.css'
 
 function Header() {
   return (
-    <header>
+    <header className='header'>
         <a href="">
             <h2>Uni List</h2>
         </a>
 
         <nav className='navbar'>
+            <input type="checkbox" id='sidebar'/>
+            <label htmlFor="sidebar">
+                <span class="material-symbols-outlined">
+                    menu_open
+                </span>
+            </label>
 
             <ul className='navList'>
+                <label htmlFor="sidebar">
+                    <span class="material-symbols-outlined">
+                        close
+                    </span>
+                </label>
+
+
+                <li className='navItem'>
+                    <a href="#" className='navLink'>
+                        <span class="material-symbols-outlined">
+                            roofing
+                        </span>
+
+                        <p>Home</p>
+                    </a>
+                </li>
+
                 <li className='navItem'>
                     <a href="" className='navLink'>
+                        <span class="material-symbols-outlined">
+                            psychology_alt
+                        </span>
 
+                        <p>About</p>
                     </a>
                 </li>
             </ul>
-
         </nav>
     </header>
   )
