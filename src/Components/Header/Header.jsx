2f5e1334e7e84296be1a3bb1df7c1021fdd9e784
Alexander-Agu <theonlyagu@gmail.com>
2024-09-15 12:34:38 +0200

    Deleted unneeded files and added my own files

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <a href="">
+            <h2>Uni List</h2>
+        </a>
+
+        <nav className='navbar'>
+
+            <ul className='navList'>
+                <li className='navItem'>
+                    <a href="" className='navLink'>
+
+                    </a>
+                </li>
+            </ul>
+
+        </nav>
+    </header>
+  )
+}
+
+export default Header
